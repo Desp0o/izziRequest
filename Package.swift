d51df810bf3308f25f320e57eb7a98cf8c133f34
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2025-04-05 19:28:03 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IzziRequest",
-            url: "https://github.com/Desp0o/izziRequest/releases/download/v1.2.0/izziRequest.xcframework.zip",
-            checksum: "1a232b7c97e493fc1640990f4e908cafb28f6848447b9ca061a9ec4636db7a79"
+            url: "https://github.com/Desp0o/izziRequest/releases/download/v2.0.0/izziRequest.xcframework.zip",
+            checksum: "7197d731e65d6a63fe8b5e05d49b5d1fbbdb6d54fca4232b8a6ae385d3def6f4"
         )
     ]
 )
